57dbe4392511d36b81f7afabce0ad5a48cd46133
Dridi Chaith <dridi.chaith@gmail.com>
2024-01-13 20:18:32 +0100

    task5: other why to get the name of the class for the new method

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -22,7 +22,7 @@ class FileStorage:
 
     def new(self, obj):
         """Sets in __objects the obj with key <obj class name>.id"""
-        key = "{}.{}".format(obj.__class__.__name__, obj.id)
+        key = "{}.{}".format(type(obj).__name__, obj.id)
         FileStorage.__objects[key] = obj
 
     def save(self):
